bfed91e6f415a4617a43fa2434d9f3a415c14500
Fernando Hernandez <ferehernandez000@gmail.com>
2020-12-02 23:10:37 -0500

    added readme skeleton

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const { addListener, title } = require('process');
+
 // array of questions for user
 const questions = [
 
@@ -14,3 +17,63 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+
+
+
+const readmeSkeleton = `# ${ /* Your Project Title */title} 
+
+## Description 
+
+${ /* Description content */title}
+
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing to the project](#Contributing to the project)
+* [Test instructions](#Test instructions)
+* [License](#license)
+* [Questions](#Questions)
+
+
+## Installation
+
+${ /* Installation instructions */title}
+
+
+## Usage 
+
+${ /* Usage instructions */title}
+
+${ /* Photos */title} optional
+
+
+## Contributing to the project
+
+${ /* Contributing content */title}
+
+
+
+## Test instructions
+
+${ /* Test instructions content */title}
+
+
+
+## License
+
+${ /* license content */title}
+
+
+
+## Questions
+
+${ /* Username */title}
+${ /* email */title}
+${ /* how to reach me description */title}
+
+
+`;
